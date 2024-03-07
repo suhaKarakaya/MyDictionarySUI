4d4a7f412b793924340eced1e99bda3416bcddbb
suhaKarakaya <suhabskarakaya@gmail.com>
2024-03-07 14:08:42 +0300

    Initial Commit

---
+++ SDictionary/SDictionaryApp.swift
@@ -0,0 +1,17 @@
+//
+//  SDictionaryApp.swift
+//  SDictionary
+//
+//  Created by Süha Karakaya on 7.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct SDictionaryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
